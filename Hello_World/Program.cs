7bb6d0ac9116dc966366ee69fe9f61e25f4800c1
emilybowersox <egbsox@gmail.com>
2020-12-10 15:33:55 -0600

    learning numbers data types

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -4,12 +4,24 @@ namespace Hello_World
 {
     class MainClass
     {
+        static int age = 28;
+        //whole numbers -128 - 127)
+        sbyte x = 300;
+
+        //variable exaple with no value: default value assigned = 0
+        static int myAge;
+
         public static void Main(string[] args)
         {
+            age = 20;
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Emily!");
             Console.WriteLine("Can I push from the terminal now?");
             Console.WriteLine("I want to commit");
+            Console.WriteLine(age);
+            Console.WriteLine(myAge);
+
         }
     }
 }
