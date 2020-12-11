43b703b046197822d30c2431c76d83564dea75e7
emilybowersox <emilygbowersox@gmail.com>
2020-12-11 12:54:50 -0600

    "fixing commit"

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -4,12 +4,21 @@ namespace Hello_World
 {
     class MainClass
     {
-        static int age = 28;
-        //whole numbers -128 - 127)
-        sbyte x = 300;
+        // sbyte = whole numbers -128 - 127)
+        // short =  whole numbers -32767 - 32767
+        // int = whole numbers ~ -2 to 2 billion
+        // long = whole numbers that are the biggest
 
-        //variable exaple with no value: default value assigned = 0
+        static float x = 99.99f;
+        //must use "f" or it will consider it a double (7 digit precision) (used in video games- processing power > accuracy) 
+        static double y = 100.994829;
+        // double = decimals up to 15 digit precision (used for real world vales except money calculations) 
+        // decimal = decialms up to 28 digit precision (used in financial applications for high level of accuracy)
+
+
+        static int age = 28;
         static int myAge;
+            //variable example with no value: default value assigned = 0
 
         public static void Main(string[] args)
         {
