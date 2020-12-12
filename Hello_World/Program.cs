d444226ae8a9bd0d099bd82d28636836252745c3
emilybowersox <emilygbowersox@gmail.com>
2020-12-11 21:47:11 -0600

    primitive data types

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -4,6 +4,10 @@ namespace Hello_World
 {
     class MainClass
     {
+        //visual code for mac notes: i'm not sure yet if logging in with my github email or pushing
+        //through just the version control tab fixed it- still need to figure our pushing
+        //through th
+
         // sbyte = whole numbers -128 - 127)
         // short =  whole numbers -32767 - 32767
         // int = whole numbers ~ -2 to 2 billion
@@ -15,10 +19,22 @@ namespace Hello_World
         // double = decimals up to 15 digit precision (used for real world vales except money calculations) 
         // decimal = decialms up to 28 digit precision (used in financial applications for high level of accuracy)
 
-
         static int age = 28;
         static int myAge;
-            //variable example with no value: default value assigned = 0
+        //variable example with no value: default value assigned = 0
+
+        //primitive data types
+
+        static bool falseBoolean = false;
+        static bool trueBoolean = true;
+
+        static char myLetter = 'A';
+        // char uses 'single quotes'
+
+        static string myName = "Emily";
+        // string uses "double quotes"
+
+
 
         public static void Main(string[] args)
         {
